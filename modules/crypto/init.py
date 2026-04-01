8eebd78c339ae51b39549f7324c4723f93976145
luxior <hanzzhanzzmc@gmail.com>
2026-04-01 13:07:20 +0700

    Create init.py

---
+++ modules/crypto/init.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Cryptography modules package for Ethical Hacker Toolkit
+Author: Jet
+GitHub: https://github.com/JettRnh
+"""
+
+from modules.crypto.hash_cracker import HashCracker
+from modules.crypto.encryption import Encryption
+
+__all__ = ['HashCracker', 'Encryption']
